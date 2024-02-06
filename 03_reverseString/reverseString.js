73d4be1b1722dd8605cade9de9cc2df5c6a18574
Elmahdi Amghar <eamghar@e2r7p9.1337.ma>
2024-02-06 19:06:33 +0100

    learning javascript

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,6 +1,10 @@
-const reverseString = function() {
-
+const reverseString = function(string) {
+    let newStr = "";
+    for(i = string.length - 1; i >= 0; i--)
+        newStr += string[i];
+    return newStr;
 };
+
 
 // Do not edit below this line
 module.exports = reverseString;
