f0bccc0a06bbcf2f6c57535a226df7d2516088f4
Elmahdi Amghar <eamghar@e2r7p9.1337.ma>
2024-02-09 16:26:46 +0100

    learning javascript

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,21 @@
-const sumAll = function() {
+const sumAll = function(first, second) {
+    if(first < 0 || second < 0 || typeof(first) !== "number" ||  typeof(second) !== "number")
+        return "ERROR";
+    let res = 0;
+    let i = 0;
 
+    if(first < second)
+        i = first;
+    else
+    {
+        let extra = second;
+        second = first;
+        first = extra;
+    }
+
+    for(; i <= second; i++)
+        res += i;
+    return res;
 };
 
 // Do not edit below this line
